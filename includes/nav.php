e930c5b4ce057e2caa8ee728c325dfeed564ea48
Ryan Rapini <ryanrapini@gmail.com>
2019-06-02 23:32:00 -0400

    Updating sunday league

--- includes/nav.php
+++ includes/nav.php
@@ -1,23 +1,37 @@
-<nav class="navbar">
-                <div class="container">
-                    <div class="navbar-brand">
-                        JAMMS Sand Volleyball
-                    </div>
-                    <div id="navbarMenu" class="navbar-menu">
-                        <div class="navbar-end">
-                            <div class="tabs is-right">
-                                <ul>
-                                    <li><a href="/">Home</a></li>
-                                    <li><a href="/#rules">Rules</a></li>
-                                    <li><a href="/#contact">Contact</a></li>
-                                    <li><a href="/#contact">Location</a></li>
-                                    <li><a href="/leagues/friday">Fridays</a></li>
-                                    <li><a href="/leagues/sunday">Sundays</a></li>
-                                    <li><a href="https://www.facebook.com/jammsvolleyball/">Facebook</a></li>
-                                    <li><a href="https://www.jammsmidnightmoonsaloon.com/">Bar Website</a></li>
-                                </ul>
+<nav class="navbar" role="navigation" aria-label="dropdown navigation">
+            <div class="container">
+                <div class="navbar-brand has-text-centered-mobile">
+                    <a class="navbar-item" href="/">JAMMS Sand Volleyball</a>
+                    <a role="button" class="navbar-burger burger" data-target="navMenu" aria-label="menu" aria-expanded="false">
+                      <span aria-hidden="true"></span>
+                      <span aria-hidden="true"></span>
+                      <span aria-hidden="true"></span>
+                    </a>
+                </div>
+                <div class="navbar-menu" id="navMenu">
+                    <div class="navbar-end">
+                        <div class="navbar-item has-dropdown is-hoverable">
+                            <a class="navbar-link">
+                                Information
+                            </a>
+                            <div class="navbar-dropdown is-right">
+                                <a class="navbar-item" href="/#rules">Rules</a>
+                                <a class="navbar-item" href="/#contact">Contact</a>
+                                <a class="navbar-item" href="/#contact">Location</a>
                             </div>
                         </div>
+                        <div class="navbar-item has-dropdown is-hoverable">
+                            <a class="navbar-link">
+                                Schedule
+                            </a>
+                            <div class="navbar-dropdown is-right">
+                                <a class="navbar-item" href="/leagues/friday">Fridays</a>
+                                <a class="navbar-item" href="/leagues/sunday">Sundays</a>
+                            </div>
+                        </div>
+                        <a class="navbar-item" href="https://www.facebook.com/jammsvolleyball/">Facebook</a>
+                        <a class="navbar-item" href="https://www.jammsmidnightmoonsaloon.com/">Bar Website</a>
                     </div>
                 </div>
-            </nav>
+            </div>
+        </nav>
--- public/leagues/sunday/index.php
+++ public/leagues/sunday/index.php
@@ -14,7 +14,7 @@
         <div class="hero-body">
          
             <div class="container has-text-centered">
-                <div class="columns is-multiline">
+                <div class="columns is-multiline is-desktop">
                     <div class="column is-12">
                         <h5 class="title is-5"><a href="index.html">Return Home</a></h5>
                     </div>
@@ -26,6 +26,7 @@
                                     <th>#</th>
                                     <th>Team Captain</th>
                                     <th>Team Name</th>
+                                    <th>Matches Played</th>
                                     <th>Games Won</th>
                                     <th>Games Lost</th>
                                 </tr>
@@ -37,33 +38,37 @@
                                     <td>Bryan Lawson</td>
                                     <td>Smash or Pass</td>
                                     <td>3</td>
+                                    <td>9</td>
                                     <td>0</td>
                                 </tr>
                                     <td>2</td>
                                     <td>Mark Anthony</td>
                                     <td>Sweet Digs</td>
-                                    <td>0</td>
                                     <td>3</td>
+                                    <td>3</td>
+                                    <td>6</td>
                                 </tr>
                                 <tr>
                                     <td>3</td>
-                                    <td>Julie Ramos</td>
-                                    <td>Team Gaz</td>
-                                    <td>0</td>
-                                    <td>0</td>
-                    
+                                    <td>Kas Beris</td>
+                                    <td>Sandy Cracks</td>
+                                    <td>3</td>
+                                    <td>2</td>
+                                    <td>7</td>
                                 </tr>
                                 <tr>
                                     <td>4</td>
                                     <td>Paul Nawrocki</td>
                                     <td>Bumpin' Uglies</td>
-                                    <td>0</td>
-                                    <td>0</td>
+                                    <td>3</td>
+                                    <td>1</td>
+                                    <td>8</td>
                                 </tr>
                                 <tr>
                                     <td>5</td>
-                                    <td>Kas Beris</td>
-                                    <td>Sandy Cracks</td>
+                                    <td>Ryan Mihalik</td>
+                                    <td>Pounders</td>
+                                    <td>0</td>
                                     <td>0</td>
                                     <td>0</td>
                                 </tr>
@@ -80,7 +85,9 @@
                                     <th>Week</th>
                                     <th>Date</th>
                                     <th>Time</th>
+                                    <th>Court</th>
                                     <th>Team Name</th>
+                                    <th width=40px></th>
                                     <th>Team Name</th>
                                 </tr>
                             </thead>
@@ -89,45 +96,271 @@
                                     <td>1</td>
                                     <td>May 19th</td>
                                     <td>4:30</td>
+                                    <td>1</td>
                                     <td>Smash or Pass</td>
+                                    <td>vs</td>
                                     <td>Sweet Digs</td>
                                 </tr>
-                                <tr><td colspan="5"></td></tr>
-                                <tr>
+                                <tr><td colspan="7"></td></tr>
+                                <tr class="completed">
                                     <td>2</td>
                                     <td>May 26th</td>
                                     <td>5:30</td>
+                                    <td>1</td>
                                     <td>Bumpin' Uglies</td>
+                                    <td>vs</td>
                                     <td>Smash or Pass</td>
                                 </tr>
-                                <tr>
-                                    <td>2</td>
-                                    <td>May 26th</td>
+                                <tr class="completed">
+                                    <td></td>
+                                    <td></td>
                                     <td>6:30</td>
+                                    <td>1</td>
                                     <td>Sweet Digs</td>
+                                    <td>vs</td>
                                     <td>Sandy Cracks</td>
                                 </tr>
-                                <tr><td colspan="5"></td></tr>
-                                <tr>
+                                <tr><td colspan="7"></td></tr>
+                                <tr class="completed">
                                     <td>3</td>
                                     <td>June 2nd</td>
                                     <td>4:30</td>
+                                    <td>1</td>
+                                    <td><strong>Bumpin' Uglies</strong></td>
+                                    <td>vs</td>
+                                    <td>Sweet Digs</td>
+                                </tr class="completed">
+                                <tr>
+                                    <td></td>
+                                    <td></td>
+                                    <td>5:30</td>
+                                    <td>1</td>
+                                    <td><strong>Bumpin' Uglies</strong></td>
+                                    <td>vs</td>
+                                    <td><strong>Sandy Cracks</strong></td>
+                                </tr>
+                                <tr class="completed">
+                                    <td></td>
+                                    <td></td>
+                                    <td>6:30</td>
+                                    <td>1</td>
+                                    <td>Smash or Pass</td>
+                                    <td>vs</td>
+                                    <td><strong>Sandy Cracks</strong></td>
+                                </tr>
+                                <tr><td colspan="7"></td></tr>
+                                <tr>
+                                    <td>4</td>
+                                    <td>June 9th</td>
+                                    <td>4:30</td>
+                                    <td>1</td>
+                                    <td>Pounders</td>
+                                    <td>vs</td>
+                                    <td><strong>Smash or Pass</strong></td>
+                                </tr>
+                                <tr>
+                                    <td></td>
+                                    <td></td>
+                                    <td>5:30</td>
+                                    <td>1</td>
                                     <td>Bumpin' Uglies</td>
+                                    <td>vs</td>
+                                    <td><strong>Smash or Pass</strong></td>
+                                </tr>
+                                <tr>
+                                    <td></td>
+                                    <td></td>
+                                    <td>6:30</td>
+                                    <td>1</td>
+                                    <td>Sweet Digs</td>
+                                    <td>vs</td>
+                                    <td>Sandy Cracks</td>
+                                </tr>
+                                <tr><td colspan="7"></td></tr>
+                                <tr>
+                                    <td>5</td>
+                                    <td>June 16th</td>
+                                    <td>4:30</td>
+                                    <td>1</td>
+                                    <td><strong>Pounders</strong></td>
+                                    <td>vs</td>
+                                    <td>Sandy Cracks</td>
+                                </tr>
+                                <tr>
+                                    <td></td>
+                                    <td></td>
+                                    <td>5:30</td>
+                                    <td>1</td>
+                                    <td><strong>Pounders</strong></td>
+                                    <td>vs</td>
+                                    <td>Bumpin' Uglies</td>
+                                </tr>
+                                <tr>
+                                    <td></td>
+                                    <td></td>
+                                    <td>6:30</td>
+                                    <td>1</td>
+                                    <td>Sweet Digs</td>
+                                    <td>vs</td>
+                                    <td>Smash or Pass</td>
+                                </tr>
+                                <tr><td colspan="7"></td></tr>
+                                <tr>
+                                    <td>6</td>
+                                    <td>June 2nd</td>
+                                    <td>4:30</td>
+                                    <td>1</td>
+                                    <td><strong>Pounders</strong></td>
+                                    <td>vs</td>
                                     <td>Sweet Digs</td>
                                 </tr>
                                 <tr>
-                                    <td>3</td>
-                                    <td>June 2nd</td>
+                                    <td></td>
+                                    <td></td>
                                     <td>5:30</td>
-                                    <td>Team Gaz</td>
+                                    <td>1</td>
+                                    <td><strong>Pounders</strong></td>
+                                    <td>vs</td>
+                                    <td>Smash or Pass</td>
+                                </tr>
+                                <tr>
+                                    <td></td>
+                                    <td></td>
+                                    <td>6:30</td>
+                                    <td>1</td>
+                                    <td>Bumpin' Uglies</td>
+                                    <td>vs</td>
+                                    <td>Sandy Cracks</td>
+                                </tr>
+                                <tr><td colspan="7"></td></tr>
+                                <tr>
+                                    <td>7</td>
+                                    <td>June 30th</td>
+                                    <td>4:30</td>
+                                    <td>1</td>
+                                    <td><strong>Pounders</strong></td>
+                                    <td>vs</td>
+                                    <td>Bumpin' Uglies</td>
+                                </tr>
+                                <tr>
+                                    <td></td>
+                                    <td></td>
+                                    <td>5:30</td>
+                                    <td>1</td>
+                                    <td><strong>Pounders</strong></td>
+                                    <td>vs</td>
+                                    <td>Sweet Digs</td>
+                                </tr>
+                                <tr>
+                                    <td></td>
+                                    <td></td>
+                                    <td>6:30</td>
+                                    <td>1</td>
+                                    <td>Smash or Pass</td>
+                                    <td>vs</td>
+                                    <td>Sandy Cracks</td>
+                                </tr>
+                                <tr><td colspan="7"></td></tr>
+                                <tr>
+                                    <td>8</td>
+                                    <td>July 7th</td>
+                                    <td>4:30</td>
+                                    <td>1</td>
+                                    <td>Pounders</td>
+                                    <td>vs</td>
+                                    <td>Sandy Cracks</td>
+                                </tr>
+                                <tr>
+                                    <td></td>
+                                    <td></td>
+                                    <td>5:30</td>
+                                    <td>1</td>
+                                    <td>Sweet Digs</td>
+                                    <td>vs</td>
+                                    <td><strong>Bumpin' Uglies</strong></td>
+                                </tr>
+                                <tr>
+                                    <td></td>
+                                    <td></td>
+                                    <td>6:30</td>
+                                    <td>1</td>
+                                    <td>Smash or Pass</td>
+                                    <td>vs</td>
+                                    <td><strong>Bumpin' Uglies</strong></td>
+                                </tr>
+                                <tr><td colspan="7"></td></tr>
+                                <tr>
+                                    <td>9</td>
+                                    <td>July 14th</td>
+                                    <td>4:30</td>
+                                    <td>1</td>
+                                    <td>Smash or Pass</td>
+                                    <td>vs</td>
                                     <td><strong>Sandy Cracks</strong></td>
                                 </tr>
                                 <tr>
-                                    <td>3</td>
-                                    <td>June 2nd</td>
-                                    <td>6:30</td>
-                                    <td>Smash or Pass</td>
+                                    <td></td>
+                                    <td></td>
+                                    <td>5:30</td>
+                                    <td>1</td>
                                     <td><strong>Sandy Cracks</strong></td>
+                                    <td>vs</td>
+                                    <td><strong>Pounders</strong></td>
+                                </tr>
+                                <tr>
+                                    <td></td>
+                                    <td></td>
+                                    <td>5:30</td>
+                                    <td>2</td>
+                                    <td>Bumpin' Uglies</td>
+                                    <td>vs</td>
+                                    <td><strong>Sweet Digs</strong></td>
+                                </tr>
+                                <tr>
+                                    <td></td>
+                                    <td></td>
+                                    <td>6:30</td>
+                                    <td>1</td>
+                                    <td><strong>Pounders</strong></td>
+                                    <td>vs</td>
+                                    <td><strong>Sweet Digs</strong></td>
+                                </tr>
+                                <tr><td colspan="7"></td></tr>
+                                <tr>
+                                    <td>10</td>
+                                    <td>July 21st</td>
+                                    <td>4:30</td>
+                                    <td>1</td>
+                                    <td>4th Place Seed</td>
+                                    <td>vs</td>
+                                    <td>5th Place Seed</td>
+                                </tr>
+                                <tr>
+                                    <td></td>
+                                    <td></td>
+                                    <td>5:30</td>
+                                    <td>1</td>
+                                    <td>Winner of 4:30</td>
+                                    <td>vs</td>
+                                    <td>1st Place Seed</td>
+                                </tr>
+                                <tr>
+                                    <td></td>
+                                    <td></td>
+                                    <td>5:30</td>
+                                    <td>2</td>
+                                    <td>2nd Place Seed</td>
+                                    <td>vs</td>
+                                    <td>3rd Place Seed</td>
+                                </tr>
+                                <tr>
+                                    <td></td>
+                                    <td></td>
+                                    <td>6:30</td>
+                                    <td>1</td>
+                                    <td>Winner Court 1</td>
+                                    <td>vs</td>
+                                    <td>Winner Court 2</td>
                                 </tr>
                             </tbody>
                         </table>
