7f43a66abe3cedbb91c3cd9d5068d182ac982de4
Ryan Rapini <ryanrapini@gmail.com>
2019-07-17 16:26:00 -0400

    fix typo in schedule

--- includes/leagues.php
+++ includes/leagues.php
@@ -7,8 +7,8 @@
             <h4 class="subtitle is-4">Wednesday - Coed 6's - $90 Per Team</h4>
             Starts <strong>July 24th</strong><br>
             Games will last 1 hour and start at either 6:30, 7:30, or 8:30<br>
-            Minimum of one girl on the court.<br>
-            Players do not need to rotate.
+            Minimum of two girls on the court.<br>
+            Players must rotate.
         </li>
         <li>
             <h4 class="subtitle is-4">Friday - Coed 4's - $60 Per Team</h4>
