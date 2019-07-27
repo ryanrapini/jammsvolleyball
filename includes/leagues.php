481b31c0c2094ef6758aa8df05e2c213f73e2460
Ryan Rapini <ryanrapini@gmail.com>
2019-07-27 01:07:17 -0400

    Remove Wednesday

--- includes/leagues.php
+++ includes/leagues.php
@@ -4,13 +4,6 @@
 <p>
     <ul>
         <li>
-            <h4 class="subtitle is-4">Wednesday - Coed 6's - $90 Per Team</h4>
-            Starts <strong>July 24th</strong><br>
-            Games will last 1 hour and start at either 6:30, 7:30, or 8:30<br>
-            Minimum of two girls on the court.<br>
-            Players must rotate.
-        </li>
-        <li>
             <h4 class="subtitle is-4">Friday - Coed 4's - $60 Per Team</h4>
             Starts <strong>July 26th</strong><br>
             Games will last 1 hour and start at either 6:30, 7:30, or 8:30<br>
