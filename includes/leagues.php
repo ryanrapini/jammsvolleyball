26a53428cd6a294ac5fb8e390ecc9b42c0ae53c1
Ryan Rapini <ryanrapini@gmail.com>
2019-07-27 18:16:28 -0400

    update

--- includes/leagues.php
+++ includes/leagues.php
@@ -2,17 +2,19 @@
     Upcoming Leagues
 </h3>
 <p>
+    For both leagues, all skill levels are accepted. If you need help finding a team, sign up as an individual player an you will be added to a team if possible.
     <ul>
         <li>
-            <h4 class="subtitle is-4">Friday - Coed 4's - $60 Per Team</h4>
-            Starts <strong>July 26th</strong><br>
+            <h4 class="subtitle is-4">Friday - Coed 4's - $15/player ($60 Per Team)</h4>
+            <h4 class="title is-4">We will be accepting signups until August 6th</h4>
             Games will last 1 hour and start at either 6:30, 7:30, or 8:30<br>
             Minimum of one girl on the court.<br>
             Players do not need to rotate.
         </li>
+        <li>&nbsp;</li>
         <li>
-            <h4 class="subtitle is-4">Sunday - Coed 6's - $90 Per Team</h4>
-            Starts <strong>July 28th</strong><br>
+            <h4 class="subtitle is-4">Sunday - Coed 6's - $15/player ($90 Per Team)</h4>
+            <h4 class="title is-4">We will be accepting signups until August 8th</h4>
             Games will last 1 hour and start at either 4:30, 5:30, or 6:30<br>
             Minimum of two girls on the court.<br>
             Players must rotate.
