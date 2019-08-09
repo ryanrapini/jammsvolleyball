8f9d16e221a1e7f0f0c0ea14e19b9e1a137d7828
Ryan Rapini <ryanrapini@gmail.com>
2019-08-09 11:05:06 -0400

    sunday schedule

--- public/leagues/sunday/index.php
+++ public/leagues/sunday/index.php
@@ -78,6 +78,15 @@
                                     <td>0</td>
                                     <td></td>
                                 </tr>
+                                <tr>
+                                    <td>6</td>
+                                    <td>Lyndsey Brandt</td>
+                                    <td>I'd Hit That</td>
+                                    <td>0</td>
+                                    <td>0</td>
+                                    <td>0</td>
+                                    <td></td>
+                                </tr>
                             </tbody>
                         </table>
                     </div>
@@ -87,6 +96,349 @@
                             A Full Schedule will be published August 8th.
                         </h5>
                         <p>For any issues with leagues including:<br> Forfeits, game reschedules, help finding substitutes, or other scheduling requests, please contact <pre>ryanrapini@gmail.com</pre></p>
+                        <table class="table is-fullwidth">
+                            <thead>
+                                <tr>
+                                    <th>Week</th>
+                                    <th>Date</th>
+                                    <th>Time</th>
+                                    <th>Court</th>
+                                    <th>Team Name</th>
+                                    <th width=40px></th>
+                                    <th>Team Name</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>1</td>
+                                    <td>July 28th</td>
+                                    <td>5:30</td>
+                                    <td>1</td>
+                                    <td>Sweet Digs</td>
+                                    <td>vs</td>
+                                    <td>Pounders</td>
+                                </tr>
+                                <tr>
+                                    <td></td>
+                                    <td></td>
+                                    <td>6:30</td>
+                                    <td>1</td>
+                                    <td>Smashed Potatoes</td>
+                                    <td>vs</td>
+                                    <td>Bumpin' Uglies</td>
+                                </tr>
+                                <tr><td colspan="7"></td></tr>
+                                <tr>
+                                    <td>2</td>
+                                    <td>August 4th</td>
+                                    <td>4:30</td>
+                                    <td>1</td>
+                                    <td>Good Sets</td>
+                                    <td>vs</td>
+                                    <td>Pounders</td>
+                                </tr>
+                                <tr>
+                                    <td></td>
+                                    <td></td>
+                                    <td>5:30</td>
+                                    <td>1</td>
+                                    <td>Sweet Digs</td>
+                                    <td>vs</td>
+                                    <td><strong>Bumpin' Uglies</strong></td>
+                                </tr>
+                                <tr>
+                                    <td></td>
+                                    <td></td>
+                                    <td>6:30</td>
+                                    <td>1</td>
+                                    <td>Smashed Potatoes</td>
+                                    <td>vs</td>
+                                    <td><strong>Bumpin' Uglies</strong></td>
+                                </tr>
+                                <tr><td colspan="7"></td></tr>
+                                <tr>
+                                    <td>3</td>
+                                    <td>August 11th</td>
+                                    <td>4:30</td>
+                                    <td>1</td>
+                                    <td>Sweet Digs</td>
+                                    <td>vs</td>
+                                    <td>Good Sets</td>
+                                </tr>
+                                <tr>
+                                    <td></td>
+                                    <td></td>
+                                    <td>5:30</td>
+                                    <td>1</td>
+                                    <td>Bumpin' Uglies</td>
+                                    <td>vs</td>
+                                    <td>Pounders</td>
+                                </tr>
+                                <tr>
+                                    <td></td>
+                                    <td></td>
+                                    <td>6:30</td>
+                                    <td>1</td>
+                                    <td>I'd Hit That</td>
+                                    <td>vs</td>
+                                    <td>Smashed Potatoes</td>
+                                </tr>
+                                <tr><td colspan="7"></td></tr>
+                                <tr>
+                                    <td>4</td>
+                                    <td>August 18th</td>
+                                    <td>4:30</td>
+                                    <td>1</td>
+                                    <td>Bumpin' Uglies</td>
+                                    <td>vs</td>
+                                    <td><strong>Sweet Digs</strong></td>
+                                </tr>
+                                <tr>
+                                    <td></td>
+                                    <td></td>
+                                    <td>5:30</td>
+                                    <td>1</td>
+                                    <td><strong>I'd Hit That</strong></td>
+                                    <td>vs</td>
+                                    <td><strong>Sweet Digs</strong></td>
+                                </tr>
+                                <tr>
+                                    <td></td>
+                                    <td></td>
+                                    <td>5:30</td>
+                                    <td>2</td>
+                                    <td>Smashed Potatoes</td>
+                                    <td>vs</td>
+                                    <td>Pounders</td>
+                                </tr>
+                                <tr>
+                                    <td></td>
+                                    <td></td>
+                                    <td>6:30</td>
+                                    <td>1</td>
+                                    <td><strong>I'd Hit That</strong></td>
+                                    <td>vs</td>
+                                    <td>Good Sets</td>
+                                </tr>
+                                <tr><td colspan="7"></td></tr>
+                                <tr>
+                                    <td>5</td>
+                                    <td>August 25th</td>
+                                    <td>4:30</td>
+                                    <td>1</td>
+                                    <td>I'd Hit That</td>
+                                    <td>vs</td>
+                                    <td>Pounders</td>
+                                </tr>
+                                <tr>
+                                    <td></td>
+                                    <td></td>
+                                    <td>5:30</td>
+                                    <td>1</td>
+                                    <td><strong>Smashed Potatoes</strong></td>
+                                    <td>vs</td>
+                                    <td>Sweet Digs</td>
+                                </tr>
+                                <tr>
+                                    <td></td>
+                                    <td></td>
+                                    <td>5:30</td>
+                                    <td>2</td>
+                                    <td>Bumpin' Uglies</td>
+                                    <td>vs</td>
+                                    <td><strong>Good Sets</strong></td>
+                                </tr>
+                                <tr>
+                                    <td></td>
+                                    <td></td>
+                                    <td>6:30</td>
+                                    <td>1</td>
+                                    <td><strong>Smashed Potatoes</strong></td>
+                                    <td>vs</td>
+                                    <td><strong>Good Sets</strong></td>
+                                </tr>
+                                <tr><td colspan="7"></td></tr>
+                                <tr>
+                                    <td>6</td>
+                                    <td>September 1st</td>
+                                    <td>4:30</td>
+                                    <td>1</td>
+                                    <td>I'd Hit That</td>
+                                    <td>vs</td>
+                                    <td>Bumpin' Uglies</td>
+                                </tr>
+                                <tr>
+                                    <td></td>
+                                    <td></td>
+                                    <td>5:30</td>
+                                    <td>1</td>
+                                    <td>Sweet Digs</td>
+                                    <td>vs</td>
+                                    <td>Pounders</td>
+                                </tr>
+                                <tr>
+                                    <td></td>
+                                    <td></td>
+                                    <td>6:30</td>
+                                    <td>1</td>
+                                    <td>Good Sets</td>
+                                    <td>vs</td>
+                                    <td>Smashed Potatoes</td>
+                                </tr>
+                                <tr><td colspan="7"></td></tr>
+                                <tr>
+                                    <td>7</td>
+                                    <td>September 8th</td>
+                                    <td>4:30</td>
+                                    <td>1</td>
+                                    <td><strong>I'd Hit That</strong></td>
+                                    <td>vs</td>
+                                    <td>Bumpin' Uglies</td>
+                                </tr>
+                                <tr>
+                                    <td></td>
+                                    <td></td>
+                                    <td>5:30</td>
+                                    <td>1</td>
+                                    <td><strong>I'd Hit That</strong></td>
+                                    <td>vs</td>
+                                    <td>Sweet Digs</td>
+                                </tr>
+                                <tr>
+                                    <td></td>
+                                    <td></td>
+                                    <td>5:30</td>
+                                    <td>2</td>
+                                    <td>Good Sets</td>
+                                    <td>vs</td>
+                                    <td><strong>Pounders</strong></td>
+                                </tr>
+                                <tr>
+                                    <td></td>
+                                    <td></td>
+                                    <td>6:30</td>
+                                    <td>1</td>
+                                    <td>Smashed Potatoes</td>
+                                    <td>vs</td>
+                                    <td><strong>Pounders</strong></td>
+                                </tr>
+                                <tr><td colspan="7"></td></tr>
+                                <tr>
+                                    <td>8</td>
+                                    <td>September 15th</td>
+                                    <td>4:30</td>
+                                    <td>1</td>
+                                    <td>Pounders</td>
+                                    <td>vs</td>
+                                    <td><strong>I'd Hit That</strong></td>
+                                </tr>
+                                <tr>
+                                    <td></td>
+                                    <td></td>
+                                    <td>5:30</td>
+                                    <td>1</td>
+                                    <td><strong>Good Sets</strong></td>
+                                    <td>vs</td>
+                                    <td><strong>I'd Hit That</strong></td>
+                                </tr>
+                                <tr>
+                                    <td></td>
+                                    <td></td>
+                                    <td>6:30</td>
+                                    <td>1</td>
+                                    <td><strong>Good Sets</strong></td>
+                                    <td>vs</td>
+                                    <td>Bumpin' Uglies</td>
+                                </tr>
+                                <tr>
+                                    <td></td>
+                                    <td></td>
+                                    <td>6:30</td>
+                                    <td>2</td>
+                                    <td>Smashed Potatoes</td>
+                                    <td>vs</td>
+                                    <td>Sweet Digs</td>
+                                </tr>
+                                <tr><td colspan="7"></td></tr>
+                                <tr>
+                                    <td>9</td>
+                                    <td>September 22nd</td>
+                                    <td>4:30</td>
+                                    <td>1</td>
+                                    <td>Good Sets</td>
+                                    <td>vs</td>
+                                    <td>Sweet Digs</td>
+                                </tr>
+                                <tr>
+                                    <td></td>
+                                    <td></td>
+                                    <td>5:30</td>
+                                    <td>1</td>
+                                    <td>Pounders</td>
+                                    <td>vs</td>
+                                    <td>Bumpin' Uglies</td>
+                                </tr>
+                                <tr>
+                                    <td></td>
+                                    <td></td>
+                                    <td>6:30</td>
+                                    <td>1</td>
+                                    <td>Smashed Potatoes</td>
+                                    <td>vs</td>
+                                    <td>I'd Hit That</td>
+                                </tr>
+                                <tr><td colspan="7"></td></tr>
+                                <tr><td colspan="7"><strong>PLAYOFFS SCHEDULE (Single Elimination, Best of Three)</strong></td></tr>
+                                <tr><td colspan="7" style="text-align: center;"><strong>Quarterfinals (Top two seeds get a bye)</strong></td></tr>
+                                <tr>
+                                    <td>10</td>
+                                    <td>September 29th</td>
+                                    <td>4:30</td>
+                                    <td>1</td>
+                                    <td>4th Seed</td>
+                                    <td>vs</td>
+                                    <td>5th Seed</td>
+                                </tr>
+                                <tr>
+                                    <td></td>
+                                    <td></td>
+                                    <td>4:30</td>
+                                    <td>2</td>
+                                    <td>3rd Seed</td>
+                                    <td>vs</td>
+                                    <td>6th Seed</td>
+                                </tr>
+                                <tr><td colspan="7" style="text-align: center;"><strong>Semifinals</strong></td></tr>
+                                <tr>
+                                    <td></td>
+                                    <td></td>
+                                    <td>5:30</td>
+                                    <td>1</td>
+                                    <td>1st Seed</td>
+                                    <td>vs</td>
+                                    <td>Winner Court 1 4:30</td>
+                                </tr>
+                                <tr>
+                                    <td></td>
+                                    <td></td>
+                                    <td>5:30</td>
+                                    <td>2</td>
+                                    <td>2nd Seed</td>
+                                    <td>vs</td>
+                                    <td>Winner Court 2 4:30</td>
+                                </tr>
+                                <tr><td colspan="7" style="text-align: center;"><strong>Finals!</strong></td></tr>
+                                <tr>
+                                    <td></td>
+                                    <td></td>
+                                    <td>6:30</td>
+                                    <td>1</td>
+                                    <td>Winner Court 1 5:30</td>
+                                    <td>vs</td>
+                                    <td>Winner Court 2 5:30</td>
+                                </tr>
+                            </tbody>
+                        </table>
                     </div>
                     <div class="column is-12">
                         <h5 class="title is-5"><a href="/">Return Home</a></h5>
